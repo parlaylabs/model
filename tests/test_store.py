4b63086a0b9792d279df1b5b7ec178b3a98aa062
Benjamin Saller <bcsaller@gmail.com>
2019-11-12 17:57:27 -0800

    update test to schema api

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,13 +1,14 @@
 import model.entity as E
 import model.store as S
+from model import schema
 
 import pytest
 
 
 def test_store_basic_example():
-     g = E.Entity.from_yaml_file(open("examples/basic/components/ghost.yaml"))
-     m = E.Entity.from_yaml_file(open("examples/basic/components/mysql.yaml"))
-     s = S.Store()
-     s.add(g)
-     s.add(m)
-     breakpoint()
+
+    s = S.Store()
+    schema.load_and_store(open("examples/basic/components/ghost.yaml"), s)
+    schema.load_and_store(open("examples/basic/components/mysql.yaml"), s)
+    assert s.qual_name["Component:ghost"]
+
