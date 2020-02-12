6af05e08df847a54c02a8f86cc94caf6b0547069
Benjamin Saller <bcsaller@gmail.com>
2020-02-12 13:19:30 -0800

    repair import after refactor

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,20 +1,20 @@
 import pytest
 
 from model import render
-from model import runtime
+from model.runtimes import kubernetes
 
 
 def test_pick_value():
     r = render.FileRenderer("-")
-    r.add("01", dict(this="test"), runtime.Kubernetes(), foo="bar")
-    r.add("02", dict(this="another"), runtime.Kubernetes(), foo="baz")
+    r.add("01", dict(this="test"), kubernetes.Kubernetes(), foo="bar")
+    r.add("02", dict(this="another"), kubernetes.Kubernetes(), foo="baz")
     result = r.pick(query={"data.this": "test"})
     assert list(result)[0].annotations["foo"] == "bar"
 
 
 def test_pick_plugin():
     r = render.FileRenderer("-")
-    r.add("01", dict(this="test"), runtime.Kubernetes(), foo="bar")
-    r.add("02", dict(this="another"), runtime.Kubernetes(), foo="baz")
-    result = r.pick(plugin=runtime.Kubernetes())
+    r.add("01", dict(this="test"), kubernetes.Kubernetes(), foo="bar")
+    r.add("02", dict(this="another"), kubernetes.Kubernetes(), foo="baz")
+    result = r.pick(plugin=kubernetes.Kubernetes())
     assert len(list(result)) == 2
